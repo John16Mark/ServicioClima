e5a2d6e73829d139aa9cf37b166ce173e7fc44d1
John16Mark <jlma-113@hotmail.com>
2023-11-21 23:53:50 -0600

    Tabla sencilla

--- js/carga.js
+++ js/carga.js
@@ -1,6 +1,25 @@
 import datos from '../json/DailyForecast_MX.json' assert {type: 'json'};
+$(document).ready(()=>{
+    
+    let filas = "";
 
-for(var i = 0; i < 5 && i<datos.length; i++){
-    var elemento = datos[i];
-    console.log(elemento);
-}
+    for(var i = 0; i < 5 || i<datos.length; i++){
+        var elemento = datos[i];
+        
+        if(elemento.nes == 'Baja California Sur'){
+            console.log(elemento);
+            filas += `
+            <tr>
+                <td>${elemento.nes}</td>
+                <td>${elemento.nmun}</td>
+                <td>${elemento.desciel}</td>
+                <td><img src='./img/${elemento.desciel}.png' class='materialboxed cambiaTamImg'></td>
+            </tr>
+            `;
+        }
+        
+    }
+    //console.log(filas);
+    $("#filasProductos").html(filas);
+    $(".materialboxed").materialbox();
+});
